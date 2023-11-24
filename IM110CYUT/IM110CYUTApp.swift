5a605cd59fc17f18d2bdcbfc483a6cc3e000b70b
hua1119 <133836033+hua1119@users.noreply.github.com>
2023-11-24 12:02:55 +0800

    Initial Commit

---
+++ IM110CYUT/IM110CYUTApp.swift
@@ -0,0 +1,20 @@
+//
+//  IM110CYUTApp.swift
+//  IM110CYUT
+//
+//  Created by Ｍac on 2023/11/24.
+//
+
+import SwiftUI
+
+@main
+struct IM110CYUTApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
