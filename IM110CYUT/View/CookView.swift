1fdab216cfb789039d1d04e39b72050110726757
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-11-26 12:05:27 +0800

    11 26

---
+++ IM110CYUT/View/CookView.swift
@@ -0,0 +1,18 @@
+//
+//  CookView.swift
+//  IM110CYUT
+//
+//  Created by Mac on 2023/11/26.
+//
+
+import SwiftUI
+
+struct CookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CookView()
+}
