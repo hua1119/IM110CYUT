27e5da498bf3adcc84e0aeb6874ee15bdd04b117
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-11-26 17:47:33 +0800

    11/26 fin

---
+++ IM110CYUT/View/AdjustView.swift
@@ -0,0 +1,18 @@
+//
+//  AdjustView.swift
+//  IM110CYUT
+//
+//  Created by Mac on 2023/11/26.
+//
+
+import SwiftUI
+
+struct AdjustView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdjustView()
+}
