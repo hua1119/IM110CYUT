6a2a8f368a7bb0618eee47804c036682c2b2655e
yanyanling <s11014069@gm.cyut.edu.tw>
2023-12-17 17:00:57 +0800

    1217 fin 增加庫存和採購完成後自動刪除

---
+++ IM110CYUT/View/StockView.swift
@@ -0,0 +1,18 @@
+//
+//  StockView.swift
+//  IM110CYUT
+//
+//  Created by Ｍac on 2023/12/17.
+//
+
+import SwiftUI
+
+struct StockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockView()
+}
