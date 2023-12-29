1e7f7b32f75ca0d39ed6d1c450e0a6e07d333060
yanyanling <s11014069@gm.cyut.edu.tw>
2023-12-29 11:56:01 +0800

    12/29 更改烹飪View

--- IM110CYUT/View/CookView.swift
+++ IM110CYUT/View/CookView.swift
@@ -1,77 +1,54 @@
-//
-//  CookView.swift
-//  IM110CYUT
-//
-//  Created by Ｍac on 2023/11/26.
-//
-
 // MARK: 烹飪View
 import SwiftUI
 
-struct Plan
-{
-    var day: Int
-    var title: String
-}
+struct CookView: View {
+    @State private var plans: [String: [String]] = [:]
 
-struct CookView: View
-{
-    @State private var fakePlans: [Plan] = []
-    
-    init() {
-        let numberOfDishes = (1...7).map { _ in Int.random(in: 1...3) }
-        var plans: [Plan] = []
-        for (index, count) in numberOfDishes.enumerated() {
-            for dishIndex in 1...count {
-                plans.append(Plan(day: index + 1, title: "料理\(dishIndex)"))
-            }
-        }
-        _fakePlans = State(initialValue: plans)
-    }
-    var body: some View
-    {
-        NavigationStack
-        {
-            VStack
-            {
+    var body: some View {
+        NavigationStack {
+            VStack {
                 Text("烹飪")
-                HStack
-                {
+                HStack {
                     Spacer()
-                    NavigationLink(destination: AdjustedView())
-                    {
+                    NavigationLink(destination: AdjustedView()) {
                         Text("調整計畫")
-//                            .padding()
-                            .offset(x: -20,y: -20)
+                            .offset(x: -20, y: -20)
                     }
                 }
-                List
-                {
+
+                List {
                     ForEach(0..<7, id: \.self) { dayIndex in
-                        VStack(alignment: .leading, spacing: 10)
-                        {
+                        VStack(alignment: .leading, spacing: 10) {
                             let currentDate = Calendar.current.dateComponents([.month, .day], from: Date())
-                            if let firstPlan = fakePlans.first(where: { $0.day == dayIndex + 1 }) {
-                                if let targetDate = Calendar.current.date(byAdding: .day, value: dayIndex, to: Date()) {
-                                    let targetDateComponents = Calendar.current.dateComponents([.month, .day], from: targetDate)
-                                    Text("\(targetDateComponents.month ?? 0)/\(targetDateComponents.day ?? 0) 第 \(dayIndex + 1) 天")
-                                        .font(.headline)
-                                        .padding(.bottom, 5)
+                            if let targetDate = Calendar.current.date(byAdding: .day, value: dayIndex, to: Date()) {
+                                let targetDateComponents = Calendar.current.dateComponents([.month, .day], from: targetDate)
+                                let dateString = "\(targetDateComponents.month ?? 0)/\(targetDateComponents.day ?? 0)"
+                                Text("\(dateString) 第 \(dayIndex + 1) 天")
+                                    .font(.headline)
+                                    .padding(.bottom, 5)
+
+                                // Check if plans for this date exist
+                                if let dayPlans = plans[dateString] {
+                                    ForEach(dayPlans, id: \.self) { plan in
+                                        Text(plan).font(.subheadline)
+                                    }
+                                } else {
+                                    Text("沒有計畫").font(.subheadline).foregroundColor(.gray)
                                 }
-                            }
-                            ForEach(fakePlans.filter { $0.day == dayIndex + 1 }, id: \.day)
-                            { plan in
-                                Text("- \(plan.title)")
-                                    .padding(.leading, 20)
                             }
                         }
                     }
                 }
+
+                .onAppear {
+                    print("CookView appeared. Refresh plans if needed.")
+                    print("Current plans: \(plans)")
+                }
+
                 .padding(.top, -20)
                 .scrollIndicators(.hidden)
-                
-                NavigationLink(destination: NowView())
-                {
+
+                NavigationLink(destination: NowView()) {
                     Text("立即煮")
                         .padding()
                 }
@@ -80,10 +57,9 @@ struct CookView: View
     }
 }
 
-struct CookView_Previews: PreviewProvider
-{
-    static var previews: some View
-    {
+
+struct CookView_Previews: PreviewProvider {
+    static var previews: some View {
         CookView()
     }
 }
