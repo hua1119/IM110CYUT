a5f8edb0ee9fc20a89b6ad19c734569bbd597c9a
yanyanyanling <s11014069@gm.cyut.edu.tw>
2024-02-22 13:10:05 +0800

    2/22 01:09

--- IM110CYUT/View/SigninView.swift
+++ IM110CYUT/View/SigninView.swift
@@ -1,124 +1,72 @@
-//
-//  SigninView.swift
-//  IM110CYUT
-//
-//  Created by Ｍac on 2023/11/24.
-//
-
-// MARK: 登入View
 import SwiftUI
 
-// MARK: 登入
-struct SigninView: View
-{
-    @AppStorage("signin") private var signin: Bool=false //存取登入狀態
+struct SigninView: View {
+    @AppStorage("signin") private var signin: Bool = false
+    @AppStorage("rememberMe") private var rememberMe: Bool = false
+    @State private var U_Acc: String = ""
+    @State private var U_Pas: String = ""
+    @State private var result: (Bool, String) = (false, "")
+    @State private var information: (String, String) = ("", "")
+    @State private var forget: Bool = false
+    @EnvironmentObject private var user: User
 
-    @State private var result: (Bool, String)=(false, "") //執行結果Alert
-    @State private var information: (String, String)=("", "") //帳號密碼
-    @State private var forget: Bool=false //開啟「忘記密碼」的狀態
-    @State private var remember: Bool=false //手機儲存「記住我」狀態，之後要改
+    private func sendRequest() {
+        let url = URL(string: "http://163.17.9.107/food/Login.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let bodyParameters = "U_Acc=\(U_Acc)&U_Pas=\(U_Pas)"
+        request.httpBody = bodyParameters.data(using: .utf8)
 
-    @EnvironmentObject private var user: User //提供所有View使用的User結構
-
-    
-    // MARK: 使用者登入
-    private func signIn() async
-    {
-        // Authentication
-        Authentication().signin(account: self.information.0, password: self.information.1)
-        { (_, error) in
-            if let error = error //登入失敗
-
-            {
-                self.result.1 = error.localizedDescription
-                self.result.0.toggle()
-            } else
-            {
-                // Realtime Database
-                RealTime().getUser(account: self.information.0)
-                { (result, error) in
-                    if let result = result //登入成功回傳的使用者資料
-                    {
-                        let dateFormatter = DateFormatter() //格式化生日日期字符串為日期
-                        dateFormatter.dateFormat = "yyyy-MM-dd" //根據你的日期格式调整
-                        
-                        // MARK: 存進User
-                        self.user.setUser(
-                            id: result[0],
-                            account: result[1],
-                            password: result[2],
-                            name: result[3],
-                            gender: result[4],
-                            birthday: result[5] ?? "", //使用轉換後的日期對象
-                            height: result[6],
-                            weight: result[7],
-                            like1: result[8],
-                            like2: result[9],
-                            like3: result[10],
-                            like4: result[11]
-                        )
-                        self.result.1 = "歡迎! \(self.user.name)"
-                        self.result.0.toggle()
-                        self.signin=true
-//                        if dateFormatter.date(from: result[5]) != nil {
-//
-//                        } else {
-//                            // 日期格式不匹配，处理错误
-//                            self.result.1 = "日期格式不匹配"
-//                            self.result.0.toggle()
-//                        }
-                        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+            } else if let data = data {
+                if let responseString = String(data: data, encoding: .utf8) {
+                    print("Response: \(responseString)")
+                    if responseString.trimmingCharacters(in: .whitespacesAndNewlines).lowercased() == "null" {
+                        result = (true, "帳號或密碼錯誤")
+                    } else {
+                        DispatchQueue.main.async {
+                            signin = true
+                            if rememberMe {
+                                // If remember me is checked, store the login state
+                                UserDefaults.standard.set(signin, forKey: "signin")
+                                UserDefaults.standard.set(U_Acc, forKey: "savedUsername")
+                                UserDefaults.standard.set(U_Pas, forKey: "savedPassword")
+                            } else {
+                                // If remember me is not checked, clear saved username and password
+                                UserDefaults.standard.removeObject(forKey: "savedUsername")
+                                UserDefaults.standard.removeObject(forKey: "savedPassword")
+                            }
+                        }
                     }
-                    else if let error = error  //登入失敗
-                    {
-                        self.result.1 = error.localizedDescription
-                        self.result.0.toggle()
-                    }
+                } else {
+                    print("Unable to decode response data.")
                 }
             }
-        }
+        }.resume()
     }
 
-    
-    var body: some View
-    {
-        NavigationStack
-        {
-            if(self.signin)
-            {
-                
+    var body: some View {
+        NavigationView {
+            if signin {
                 ContentView().transition(.opacity)
-            }
-            else
-            {
-                VStack(spacing: 20)
-                {
-                    //MARK: logo
-//                                        Image("bitter")
-//                                            .resizable()
-//                                            .scaledToFit()
-//                                            .frame(width: 150)
-//                                            .background(.gray)
-//                                            .clipShape(Circle())
-//                                            .padding(.bottom, 50)
-//                    
+            } else {
+                VStack(spacing: 20) {
                     Circle()
                         .fill(.gray)
                         .scaledToFit()
                         .frame(width: 150)
                         .padding(.bottom, 50)
                     
-                    VStack(spacing: 30)
-                    {
-                        // MARK: 帳號
-                        TextField("帳號...", text: self.$information.0)
+                    VStack(spacing: 30) {
+                        TextField("帳號...",text: $U_Acc)
                             .scrollContentBackground(.hidden)
                             .padding()
                             .background(Color(.systemGray5))
                             .clipShape(Capsule())
                         
-                        // MARK: 密碼
-                        SecureField("密碼...", text: self.$information.1)
+                        SecureField("密碼...",text: $U_Pas)
                             .scrollContentBackground(.hidden)
                             .padding()
                             .background(Color(.systemGray5))
@@ -126,63 +74,51 @@ struct SigninView: View
                     }
                     .font(.title3)
                     
-                    NavigationLink(destination:SignupView(textselect: .constant(0)))
-                    {
+                    NavigationLink(destination: SignupView(textselect: .constant(0))) {
                         Text("尚未註冊嗎？請點擊我")
                             .font(.body)
                             .foregroundColor(Color(red: 0.574, green: 0.609, blue: 0.386))
                             .colorMultiply(.gray)
                     }
-                    HStack
-                    {
-                        // MARK: 記住我
-                        HStack
-                        {
+                    
+                    HStack {
+                        HStack {
                             Circle()
                                 .fill(Color(.systemGray6))
                                 .frame(width: 20)
-                                .overlay
-                            {
-                                Circle()
-                                    .fill(.blue)
-                                    .padding(5)
-                                    .opacity(self.remember ? 1:0)
-                            }
-                            .onTapGesture
-                            {
-                                withAnimation(.easeInOut)
-                                {
-                                    self.remember.toggle()
+                                .overlay {
+                                    Circle()
+                                        .fill(.blue)
+                                        .padding(5)
+                                        .opacity(rememberMe ? 1 : 0)
                                 }
-                            }
+                                .onTapGesture {
+                                    withAnimation(.easeInOut) {
+                                        rememberMe.toggle()
+                                    }
+                                }
                             
                             Text("記住我").font(.callout)
                         }
                         
                         Spacer()
                         
-                        // MARK: 忘記密碼
-                        Button("忘記密碼？")
-                        {
-                            self.forget.toggle()
+                        Button("忘記密碼？") {
+                            forget.toggle()
                         }
                         .font(.callout)
                     }
-                    .sheet(isPresented: self.$forget)
-                    {
+                    .sheet(isPresented: $forget) {
                         ForgetPasswordView()
                             .presentationDetents([.medium])
                             .presentationCornerRadius(30)
                     }
-                    // MARK: 登入
-                    Button
-                    {
+                    
+                    Button {
                         Task {
-                            await self.signIn() //登入
+                            sendRequest()
                         }
-                    }
-                label:
-                    {
+                    } label: {
                         Text("登入")
                             .font(.title3)
                             .foregroundColor(.white)
@@ -192,56 +128,48 @@ struct SigninView: View
                             .clipShape(Capsule())
                     }
                     
-                    // MARK: Circle
-                    HStack
-                    {
-                        ForEach(0..<3)
-                        {index in
+                    HStack {
+                        ForEach(0..<3) { index in
                             Circle()
                                 .fill(Color(.systemGray3))
                                 .scaledToFit()
                                 .frame(height: 50)
                             
-                            if(index<2)
-                            {
+                            if index < 2 {
                                 Spacer()
                             }
                         }
                     }
                 }
                 .onTapGesture {
-                    self.dismissKeyboard()
+                    dismissKeyboard()
                 }
                 .padding(.horizontal, 50)
                 .transition(.opacity)
                 .ignoresSafeArea(.keyboard)
             }
-                
         }
-        // MARK: 結果Alert
-        .alert(self.result.1, isPresented: self.$result.0)
-        {
-            Button("完成", role: .cancel)
-            {
-                if(self.result.1.hasPrefix("歡迎"))
-                {
-                    withAnimation(.easeInOut.speed(2))
-                    {
-                        self.signin=true
+        .alert(result.1, isPresented: $result.0) {
+            Button("完成", role: .cancel) {
+                if result.1.hasPrefix("歡迎") {
+                    withAnimation(.easeInOut.speed(2)) {
+                        signin = true
                     }
                 }
             }
         }
+        // 在视图初始化时加载保存的用户名和密码
+        .onAppear {
+            let savedUsername = UserDefaults.standard.string(forKey: "savedUsername") ?? ""
+            let savedPassword = UserDefaults.standard.string(forKey: "savedPassword") ?? ""
+            self.U_Acc = savedUsername
+            self.U_Pas = savedPassword
+        }
     }
-        
 }
-    
 
-
-struct SigninView_Previews: PreviewProvider
-{
-    static var previews: some View
-    {
+struct SigninView_Previews: PreviewProvider {
+    static var previews: some View {
         SigninView()
     }
 }
